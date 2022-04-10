57d88bf44c54fd651ecc6c21550fdac9bfb7609a
Rnad95 <r.khawatrah13@gmail.com>
2022-04-10 15:48:56 +0300

    Adding some setting and properities

---
+++ demo/src/main/java/com/example/utilityserviceprovider/UtilityServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.utilityserviceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilityServiceProviderApplication.class, args);
+    }
+
+}

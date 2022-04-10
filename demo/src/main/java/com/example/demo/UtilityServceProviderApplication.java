1b3526b1f3eff2bdec23cdc13fee303ec0c62ba7
Rnad95 <r.khawatrah13@gmail.com>
2022-04-10 15:15:35 +0300

    Creating Inital Project

---
+++ demo/src/main/java/com/example/demo/UtilityServceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityServceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityServceProviderApplication.class, args);
+	}
+
+}

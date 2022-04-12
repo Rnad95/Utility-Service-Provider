b44e70d9ae154b8ecf156af893a561d636074688
Rnad95 <r.khawatrah13@gmail.com>
2022-04-12 13:49:42 +0300

    fixing the direcories

--- demo/src/main/java/com/example/utilityserviceprovider/repositories/ServiceRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.utilityserviceprovider.repositories;
-
-import com.example.utilityserviceprovider.models.ServiceRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceRequestRepository extends JpaRepository<ServiceRequest, Integer> {
-}

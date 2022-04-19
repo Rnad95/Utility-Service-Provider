51940a768e75d4ae50ce78755f69ae4068ebf702
Rnad95 <r.khawatrah13@gmail.com>
2022-04-19 10:36:18 +0300

    Adding Renad's work

--- demo/src/main/java/com/example/utilityserviceprovider/web/SearchController.java
+++ demo/src/main/java/com/example/utilityserviceprovider/web/SearchController.java
@@ -1,20 +1,22 @@
 package com.example.utilityserviceprovider.web;
 
 import com.example.utilityserviceprovider.domain.MyUser;
+import com.example.utilityserviceprovider.domain.Role;
+import com.example.utilityserviceprovider.domain.ServiceRequest;
 import com.example.utilityserviceprovider.infrastructure.MyUserRepo;
 import com.example.utilityserviceprovider.infrastructure.RoleRepo;
 import com.example.utilityserviceprovider.services.ProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
 
@@ -35,12 +37,11 @@ public class SearchController {
 //        return "search";
 //    }
 
-//
-//    @RequestMapping("/render")
-//    String viewRenderPage(Model model, @Param("keyword") String keyword){
-//        List<MyUser> listProvider = providerService.listAll(keyword);
-//        model.addAttribute("SerProvList",listProvider);
-//        return "render-sp";
+//    @GetMapping("/allProvider")
+//    public String getAllProvider (Model model){
+//        List<MyUser> listProvider = myUserRepo.findAll();
+//        model.addAttribute("SerProvList", listProvider);
+//        return "search";
 //    }
 
 
@@ -51,11 +52,11 @@ public class SearchController {
         return "render-sp";
     }
 
-    public String getRequestForm (Model model ,@PathVariable Long id){
-        MyUser provider = myUserRepo.findById(id).orElseThrow();
-        model.addAttribute("provider", provider);
-        return "request.html";
-    }
+    //    public String getRequestForm (Model model ,@PathVariable Long id){
+//        MyUser provider = myUserRepo.findById(id).orElseThrow();
+//        model.addAttribute("provider", provider);
+//        return "request.html";
+//    }
     @GetMapping("/profile/{id}")
     public String getHome(Principal p , Model m, @PathVariable Long id){
         if(p!=null){
